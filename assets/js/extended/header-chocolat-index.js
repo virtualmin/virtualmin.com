9e6b0a78b15d7523d8fa7028b9a6d0603bbebaa8
Ilia Ross <ilia@ross.gdn>
2024-02-12 13:57:42 +0200

    Index page: Add support for gallery carousel

---
+++ assets/js/extended/header-chocolat-index.js
@@ -0,0 +1,92 @@
+// Chocolat on index page
+window.addEventListener("DOMContentLoaded", () => {
+    const themeToggle = document.querySelector("#theme-toggle"),
+        getScreenshots = function () {
+            const screenshotType =
+                localStorage.getItem("pref-theme") === "dark" ||
+                document.body.classList.contains("dark")
+                    ? "dark"
+                    : "light";
+            return [
+                {
+                    src: "/images/screenshots/" + screenshotType + "/1-dashboard.png",
+                    title: "Webmin ⇾ System ⇾ Dashboard",
+                },
+                {
+                    src: "/images/screenshots/" + screenshotType + "/2-stats-history.png",
+                    title: "Webmin ⇾ System ⇾ Dashboard ⇾ Stats History",
+                },
+            ];
+        };
+
+    // Prepare to initialize Chocolat
+    const insertFullScreenToggle = function () {
+            const fullscreenCont = document.createElement("div"),
+                fullscreenIcon = document.createElement("i");
+            fullscreenCont.classList.add("screen-full-container");
+            fullscreenCont.setAttribute("aria-label", "Enter Full Screen");
+            fullscreenIcon.classList.add("wm", "wm-sm", "wm-fw", "wm-expand");
+            fullscreenCont.appendChild(fullscreenIcon);
+            document.querySelector(".chocolat-image-wrapper").appendChild(fullscreenCont);
+            document.querySelector(".screen-full-container").addEventListener(
+                "click",
+                function (e) {
+                    e.stopPropagation();
+                    document.querySelector(".chocolat-bottom .chocolat-fullscreen").click();
+                },
+                false
+            );
+        },
+        chocolatOptions = function () {
+            return {
+                container: document.querySelector(".chocolat-drop"),
+                loop: true,
+                imageSize: "scale-down",
+                closeOnBackgroundClick: false,
+            };
+        };
+
+    document.addEventListener("keydown", function (event) {
+        if (event.key === "Escape") {
+            event.preventDefault();
+            event.stopPropagation();
+            event.stopImmediatePropagation();
+        }
+    });
+
+    // Initialize Chocolat
+    let chocolat = Chocolat(getScreenshots(), chocolatOptions());
+    chocolat.api.open();
+
+    // Insert custom open in full screen toggle
+    insertFullScreenToggle();
+
+    // If changing theme, reload the page to update screenshots
+    themeToggle.addEventListener(
+        "click",
+        function () {
+            const currIndex = chocolat.api.current();
+            chocolat.destroy();
+            chocolat = Chocolat(getScreenshots(), chocolatOptions());
+            chocolat.api.open(currIndex || 0);
+            insertFullScreenToggle();
+        },
+        false
+    );
+
+    // On resize accommodate Chocolat viewer
+    window.onresize = function () {
+        const targetClassList = document.querySelector("html").classList;
+        if (
+            document.fullscreenElement ||
+            document.webkitCurrentFullScreenElement ||
+            document.webkitFullscreenElement
+        ) {
+            targetClassList.add("chocolat-fullscreen");
+        } else {
+            targetClassList.remove("chocolat-fullscreen");
+            chocolat.api.set("imageSize", "contain");
+            chocolat.api.position();
+        }
+    };
+});
