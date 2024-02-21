32dfd88aeffdfa700ec1c4727670e46e7393bb27
Ilia Ross <ilia@ross.gdn>
2024-02-22 00:19:25 +0200

    Index page: Add styling to features

--- assets/js/extended/footer_.js
+++ assets/js/extended/footer_.js
@@ -1,1 +1,14 @@
 /* jshint esversion: 6 */
+
+// On resize event
+window.addEventListener("resize", function () {
+    // Set the height of the footer
+    const blockSeparatorSelStr = ".index-page-intro-separator-block",
+        leftOffset = document.querySelector(".post-content").getBoundingClientRect().left,
+        documentWidth = document.documentElement.getBoundingClientRect().width;
+    document.querySelector(blockSeparatorSelStr).style.width = documentWidth + "px";
+    document.querySelector(blockSeparatorSelStr).style.marginLeft = -leftOffset + "px";
+});
+
+// Trigger resize on load
+window.dispatchEvent(new Event("resize"));
