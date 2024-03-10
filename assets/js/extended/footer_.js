223d034f317c536e7343efaf9e07f6b20c9e3a28
Ilia Ross <ilia@ross.gdn>
2024-03-10 13:22:27 +0200

    Fix code load scope

--- assets/js/extended/footer_.js
+++ assets/js/extended/footer_.js
@@ -1,18 +1,2 @@
+
 /* jshint esversion: 6 */
-
-// On resize event
-window.addEventListener("resize", function () {
-    // Set the height of the footer
-    const blockSeparatorSelStr = ".index-page-intro-separator-block",
-        leftOffset = document.querySelector(".post-content").getBoundingClientRect().left,
-        documentWidth = document.documentElement.getBoundingClientRect().width,
-        blockSeparators = document.querySelectorAll(blockSeparatorSelStr);
-
-    blockSeparators.forEach((blockSeparator) => {
-        blockSeparator.style.width = documentWidth + "px";
-        blockSeparator.style.marginLeft = -leftOffset + "px";
-    });
-});
-
-// Trigger resize on load
-window.dispatchEvent(new Event("resize"));
